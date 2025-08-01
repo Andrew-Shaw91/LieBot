b764c2ab535e222c1ca4b14c0fa2674a59ff4a7c
Ah.hi_hi <85371640+ah-hi-hi@users.noreply.github.com>
2025-08-02 00:32:46 +0100

    Update models.py
    most of the models implemented

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Chat(models.Model):
+  prompt = models.CharField(max_length = 255)
+
+class Answer(models.Model):
+  # answer = 
+
+class User(models.Model):
+  username = models.CharField(max_length=30)
+
+class Account(models.Model):
+  username = User.username
+  password = models.CharField(max_length=30)
