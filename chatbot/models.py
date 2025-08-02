1a7101637d1fb20768693a9809945e0701d93394
Ah.hi_hi <85371640+ah-hi-hi@users.noreply.github.com>
2025-08-02 01:06:43 +0100

    Update models.py

--- chatbot/models.py
+++ chatbot/models.py
@@ -2,13 +2,9 @@ from django.db import models
 
 class Chat(models.Model):
   prompt = models.CharField(max_length = 255)
-
-class Answer(models.Model):
-  # answer = 
-
-class User(models.Model):
-  username = models.CharField(max_length=30)
+  answer = models.CharField(max_length = 255)
 
 class Account(models.Model):
-  username = User.username
-  password = models.CharField(max_length=30)
+  username = models.CharField(max_length = 30)
+  password = models.CharField(max_length = 30)
+  savedChats = models.ForeignKey(Chat, on_delete = models.CASCADE)
